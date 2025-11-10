7a84f656b8b76623233950e93cfe828c4d69472c
diegorodriguez70 <156062744+diegorodriguez70@users.noreply.github.com>
2025-11-10 16:11:11 +0100

    Cupon services copiado y renombrado

--- GastroMap_proyecto_final_diegorf70-main/proyecto/src/main/java/com/proyecto/services/CuponService.java
+++ GastroMap_proyecto_final_diegorf70-main/proyecto/src/main/java/com/proyecto/services/CuponService.java
@@ -4,5 +4,38 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CuponService {
+@Autowired
+	CuponRepository cuponRepository;
+	
+	public Optional<Cupon> getCuponOptionalById(int idCupon){
+		
+		Optional<Cupon> cuponOptional = cuponRepository.findById(idCupon);
+		if(cuponOptional.isPresent()) {
+			return cuponOptional;
+		}
+		//lanzar una excepcion
+		return null;
+	}
 
+	public void deleteCuponById(int idCupon) {
+		try {
+			cuponRepository.deleteById(idCupon);
+		}catch(Exception e) {
+			System.out.println("no existe el cupon que quieres borrar");
+		}
+		
+	}
+	public void saveCupon (Cupon cupon) {
+		
+		cuponRepository.save(cupon);
+	}
+	
+	/*public void updateCuponById(int idCupon) {
+		
+		Optional<Cupon> cupon = cuponRepository.findById(idCupon);
+		if(cupon.isPresent()) {
+			
+			
+		}
+	}*/
 }
